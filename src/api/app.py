9376727760c4600254af8b005426d77a22536c44
uchaitukiran <uchaitukiran@gmail.com>
2026-04-25 13:12:45 +0530

    Disabled background RAG load to prevent crashes

--- src/api/app.py
+++ src/api/app.py
@@ -80,9 +80,9 @@ def load_rag_models_background():
         print(f"⚠️ Background Loader Failed: {e}")
 
 # Start the thread immediately when script loads
-rag_thread = threading.Thread(target=load_rag_models_background)
-rag_thread.daemon = True
-rag_thread.start()
+#rag_thread = threading.Thread(target=load_rag_models_background)
+#rag_thread.daemon = True
+#rag_thread.start()
 
 # Routes for Webpage
 @app.route("/", methods=['GET'])
